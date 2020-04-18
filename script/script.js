ba4148d379c78762aed360c76d1c740985202641
darkj0n <sadolf12@gmail.com>
2020-04-18 04:01:00 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var desturla = new Audio("src/sound.m4a");
+audio.oncanplaythrough = function(){}
+audio.onended = function(){}
+
+function degistir() {
+    document.getElementById("degis").src = "src/feels.png";
+}
